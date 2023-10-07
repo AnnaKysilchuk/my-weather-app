fdc3b195bb7c98f85214820d6aaa7af183b0b83b
Anna Kysilchuk <anya.kislchuk@gmail.com>
2023-10-07 19:11:57 +0300

    forecast function added

--- src/index.js
+++ src/index.js
@@ -29,6 +29,21 @@ function getDateInfo(dt) {
   return `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]}`;
 }
 
+function getForecastDay(dt) {
+  let date = new Date(dt * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${days[date.getDay()]}`;
+  // return `${days[date.getDay() + i]}`;
+}
 /* Time changing*/
 
 function getTimeInfo(dt) {
@@ -57,6 +72,7 @@ let numberOfClickC = 2;
 let apiKey = "e34d0t2732c4955489449b41af8fo3a4";
 // let apiKey = "ebef9ca4a8de66ed586fac628fade056";
 let apiUrlBase = `https://api.shecodes.io/weather/v1/current?&key=${apiKey}`;
+let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?key=${apiKey}`;
 
 // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${apiKey}`;
 
@@ -64,16 +80,17 @@ function currentPosition(position) {
   let currentLat = position.coords.latitude;
   let currentLong = position.coords.longitude;
   apiUrl = `${apiUrlBase}&lon=${currentLong}&lat=${currentLat}`;
+  urlForecast = `${apiUrlForecast}&lon=${currentLong}&lat=${currentLat}`;
   // apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${apiKey}&lat=${currentLat}&lon=${currentLong}`;
 
   axios.get(apiUrl).then(getWeatherInfo);
+  axios.get(urlForecast).then(getForecastWeatherInfo);
 }
 
 function getWeatherInfo(response) {
   let cityName = document.querySelector(".city-name");
   let countryName = document.querySelector(".weather-info_country");
 
-  let temperatureFeeling = document.querySelector(".temperature");
   let description = document.querySelector(".description_val");
   let wind = document.querySelector(".wind_val");
   let humidity = document.querySelector(".humidity_val");
@@ -101,14 +118,42 @@ function getWeatherInfo(response) {
   currentTimeInfo.innerHTML = getTimeInfo(response.data.time * 1000);
   // currentTimeInfo.innerHTML = getTimeInfo(response.data.dt * 1000);
 
-  console.log(response.data);
-
   weatherIcon.setAttribute("src", `images/${response.data.condition.icon}.svg`);
   weatherIcon.setAttribute("alt", `${response.data.condition.icon}`);
   // weatherIcon.setAttribute(
   //   "src",
   //   `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
   // );
+}
+
+function getForecastWeatherInfo(response) {
+  let weatherWeek = document.querySelector(".weather-week");
+  let responseArr = response.data.daily;
+  let weatherWeekHTML = "";
+
+  responseArr.forEach(function (item, index) {
+    if (index > 0) {
+      weatherWeekHTML += `<div class="row">
+      <div class="col-4 d-flex align-items-center day-name">
+        ${getForecastDay(item.time)}
+      </div>
+      <div class="col-2">
+        <img src="images/${item.condition.icon}.svg" alt="${
+        item.condition.icon
+      }" />
+      </div>
+      <div class="col d-flex align-items-center justify-content-center">
+        ${Math.round(item.temperature.maximum)}°
+      </div>
+      <div class="col d-flex align-items-center">${Math.round(
+        item.temperature.minimum
+      )}°
+      </div>
+    </div>`;
+    }
+  });
+
+  weatherWeek.innerHTML = weatherWeekHTML;
 }
 
 function inputFormSubmit(event) {
@@ -120,10 +165,12 @@ function inputFormSubmit(event) {
     newCityToSeatch += city.value[i].toLowerCase();
   }
   apiUrl = `${apiUrlBase}&query=${newCityToSeatch}`;
+  urlForecast = `${apiUrlForecast}&query=${newCityToSeatch}`;
 
   // apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${apiKey}&q=${newCityToSeatch}`;
 
   axios.get(apiUrl).then(getWeatherInfo);
+  axios.get(urlForecast).then(getForecastWeatherInfo);
 
   city.value = "";
 }
